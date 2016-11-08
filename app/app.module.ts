778822393ec61b09d455810a0ce1b1d19ab9fc51
Rochelle Lewis <rlewis37@cnm.edu>
2016-11-07 21:31:56 -0700

    add app/components/splash-component.ts - bare bones and remove all refs to diceware component/service in app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,7 +4,6 @@ import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
-import {DicewareService} from "./services/diceware-service";
 
 const moduleDeclarations = [AppComponent];
 
@@ -12,6 +11,6 @@ const moduleDeclarations = [AppComponent];
 	imports:      [BrowserModule, FormsModule, HttpModule, routing],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
-	providers:    [appRoutingProviders, DicewareService]
+	providers:    [appRoutingProviders]
 })
 export class AppModule {}
---
+++ app/components/splash-component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+	templateUrl: "./templates/splash.php"
+})
+
+export class SplashComponent {
+
+}
