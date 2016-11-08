5eb0011827b6bfc45812808b03a06958b93d6c63
Rochelle Lewis <rlewis37@cnm.edu>
2016-11-07 22:58:25 -0700

    webpack font awesome not working - removing icon

--- public_html/templates/splash.php
+++ public_html/templates/splash.php
@@ -2,4 +2,4 @@
 <hr>
 <p>So this is a totally stripped-down version of the "Getting Started with Angular 2" screencast. What's different, you ask? All the back end logic that is specific to the Diceware App has been stripped out, and this is just a pristine front-end with only one view component for this single page alone. Voila!</p>
 <p>Read the detailed step-by-step instructions in the README.md file.</p>
-<p><i class="fa fa-github fa-lg"></i>  View or fork this on GitHub: <a href="https://github.com/rlewis2892/new-ng2" target="_blank">https://github.com/rlewis2892/new-ng2</a></p>
+<p>View or fork this on GitHub: <a href="https://github.com/rlewis2892/new-ng2" target="_blank">https://github.com/rlewis2892/new-ng2</a></p>
